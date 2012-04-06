f3ce4d8e948908d2e25ede2da9b59c0bce22636d
Thomas Badie <badie@lrde.epita.fr>
2012-04-06 13:40:53 +0200

    * src/tgbatest/spotlbtt.test: Add the simulation. * src/tgbatest/ltl2tgba.cc: Add direct simulation.

--- src/tgbatest/ltl2tgba.cc
+++ src/tgbatest/ltl2tgba.cc
@@ -66,6 +66,7 @@
 #include "tgbaalgos/emptiness_stats.hh"
 #include "tgbaalgos/scc.hh"
 #include "kripkeparse/public.hh"
+#include "tgbaalgos/simulation.hh"
 
 std::string
 ltl_defs()
@@ -348,6 +349,8 @@ main(int argc, char** argv)
   bool use_timer = false;
   bool assume_sba = false;
   bool opt_simul = false;
+  bool reduction_dir_sim = false;
+  spot::tgba* temp_dir_sim = 0;
 
   for (;;)
     {
@@ -630,6 +633,10 @@ main(int argc, char** argv)
 	{
 	  reduc_aut |= spot::Reduce_quotient_Dir_Sim;
 	}
+      else if (!strcmp(argv[formula_index], "-RSD"))
+        {
+          reduction_dir_sim = true;
+        }
       else if (!strcmp(argv[formula_index], "-R1t"))
 	{
 	  reduc_aut |= spot::Reduce_transition_Dir_Sim;
@@ -1058,6 +1065,15 @@ main(int argc, char** argv)
 				// pointless.
 	}
 
+
+      if (reduction_dir_sim)
+        {
+          tm.start("Reduction w/ direct simulation");
+          temp_dir_sim = spot::simulation(a);
+          a = temp_dir_sim;
+        }
+
+
       spot::tgba_reduc* aut_red = 0;
       if (reduc_aut != spot::Reduce_None)
 	{
@@ -1440,6 +1456,7 @@ main(int argc, char** argv)
       delete state_labeled;
       delete to_free;
       delete echeck_inst;
+      delete temp_dir_sim;
     }
   else
     {
