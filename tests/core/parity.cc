yanntm/spot

parity: Add spot::colorize_parity()
These functions colorize automata with parity acceptance. They output
parity automata.

* spot/twaalgos/parity.cc, spot/twaalgos/parity.hh: Here
* tests/core/parity.cc: Add tests for spot::colorize_parity()
* tests/python/parity.ipynb: Add documentation about
spot::colorize_parity()

tests/core/parity.cc
@@ -304,6 +304,20 @@ static bool is_colorized(spot::const_twa_graph_ptr aut, bool hard)
   return true;
 }
 
+static spot::parity_order to_parity_order(bool is_max)
+{
+  if (is_max)
+    return spot::parity_order_max;
+  return spot::parity_order_min;
+}
+
+static spot::parity_style to_parity_style(bool is_odd)
+{
+  if (is_odd)
+    return spot::parity_style_odd;
+  return spot::parity_style_even;
+}
+
 int main()
 {
   auto current_bdd = spot::make_bdd_dict();
@@ -358,6 +372,21 @@ int main()
                   assert(is_colorized(output, false)
                          && "change_parity: too many acc on a transition");
                 }
+            // Check colorize_parity
+            for (auto keep_style: { true, false })
+              {
+                auto output = spot::colorize_parity(aut, keep_style);
+                assert(is_colorized(output, true)
+                       && "colorize_parity: not colorized.");
+                assert(are_equiv(aut, output)
+                       && "colorize_parity: not equivalent.");
+                auto target_order = to_parity_order(is_max);
+                auto target_style = keep_style ? to_parity_style(is_odd)
+                                    : spot::parity_style_dontcare;
+                assert(is_right_parity(output, target_order, target_style,
+                                       is_max, is_odd, acc_num_sets)
+                       && "change_parity: wrong acceptance.");
+              }
           }
         }
   return 0;

hekmatnejad/mvspot

tests/core/parity.cc
@@ -304,6 +304,20 @@ static bool is_colorized(spot::const_twa_graph_ptr aut, bool hard)
   return true;
 }
 
+static spot::parity_kind to_parity_kind(bool is_max)
+{
+  if (is_max)
+    return spot::parity_kind_max;
+  return spot::parity_kind_min;
+}
+
+static spot::parity_style to_parity_style(bool is_odd)
+{
+  if (is_odd)
+    return spot::parity_style_odd;
+  return spot::parity_style_even;
+}
+
 int main()
 {
   auto current_bdd = spot::make_bdd_dict();
@@ -358,6 +372,21 @@ int main()
                   assert(is_colorized(output, false)
                          && "change_parity: too many acc on a transition");
                 }
+            // Check colorize_parity
+            for (auto keep_style: { true, false })
+              {
+                auto output = spot::colorize_parity(aut, keep_style);
+                assert(is_colorized(output, true)
+                       && "colorize_parity: not colorized.");
+                assert(are_equiv(aut, output)
+                       && "colorize_parity: not equivalent.");
+                auto target_kind = to_parity_kind(is_max);
+                auto target_style = keep_style ? to_parity_style(is_odd)
+                                    : spot::parity_style_any;
+                assert(is_right_parity(output, target_kind, target_style,
+                                       is_max, is_odd, acc_num_sets)
+                       && "change_parity: wrong acceptance.");
+              }
           }
         }
   return 0;